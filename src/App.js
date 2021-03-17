90c656371d281e2e0ab03a55ed60a214c76b04bc
Aryamaan Kunwar <aryamaan3@gmail.com>
2021-03-17 13:42:59 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chat app</h1>
+
+      {/*Discussions*/}
+
+      {/*Chat*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Discussions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Discussions() {
+    return (
+        <div className='discussions'>
+            
+        </div>
+    )
+}
+
+export default Discussions
