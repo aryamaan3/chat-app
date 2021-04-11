e273316fbc63518db4ec99606e74e63fd7993e0d
Aryamaan Kunwar <aryamaan3@gmail.com>
2021-04-11 21:31:15 +0200

    fixed chat onload bug

--- frontend/src/Discussions.js
+++ frontend/src/Discussions.js
@@ -1,17 +1,15 @@
 import { Avatar } from '@material-ui/core';
 import { SearchOutlined } from '@material-ui/icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Discussions.css";
 import ListeDiscussion from './ListeDiscussion.js';
 
 function Discussions({username}) {
-    const letter1 = username[0]
-    const letter2 = username[1]
 
     return (
         <div className='discussions'>
             <div className='discussions__enTete'>
-                <Avatar>{letter1 + letter2}</Avatar>
+                <Avatar>{username}</Avatar>
                 <div className="discussions__enTeteDroite">
                     {username}
                 </div>
